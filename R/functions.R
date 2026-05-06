999a2a5c3a931bf12f30428d55d11de36f620b9b
Freja Gram <frejagr@hotmail.com>
2026-05-06 12:21:28 +0200

    Create new function to find id from filepath and added packages to desription

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,3 @@
-
 #' Read in one nurses' stress data file
 #'
 #' @param a path to a data file
@@ -8,10 +7,50 @@
 
 read <- function(file.path, max_rows = 100) {
   data <- file.path |>
-    readr::read_csv( #readr lokaliserer hvilken pakke der anvendes
+    readr::read_csv( # readr lokaliserer hvilken pakke der anvendes
       show_col_types = FALSE,
       name_repair = snakecase::to_snake_case,
       n_max = max_rows
     )
   base::return(data)
+}
+
+
+#' Read in all files in nurses stress
+#'
+#' @param filename
+#'
+#' @returns outputs a dataframe
+
+read_all <- function(filename) {
+  files <- here::here("data-raw/nurses-stress/") %>%
+    fs::dir_ls(regexp = filename, recurse = TRUE)
+
+  data <- files %>%
+    purrr::map(read) %>%
+    purrr::list_rbind(names_to = "file_path_id")
+
+  return(data)
+}
+
+
+#' Get IDs from file_path_id
+#'
+#' @param data
+#'
+#' @returns outputs a new column with ID
+
+get_participant_id <- function(data) {
+  data <- data %>%
+    dplyr::mutate(
+      id = str_extract(
+        file_path_id,
+        pattern = "/stress/[:alnum:]{2}/"
+      ) %>%
+        stringr::str_remove("/stress/") %>%
+        stringr::str_remove("/"),
+      .before = file_path_id
+    ) %>%
+    dplyr::select(-file_path_id)
+  return(data)
 }
